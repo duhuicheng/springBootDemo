7e52e115a4e112df8c21f47f8cc49d45d75baffe
1095989419@qq.com <1095989419@qq.com>
2019-08-07 23:05:14 +0800

    新增getParameterMap方法用于接收请求数据

--- src/main/java/com/springBoot/demo/controller/TestController.java
+++ src/main/java/com/springBoot/demo/controller/TestController.java
@@ -2,21 +2,29 @@ package com.springBoot.demo.controller;
 
 import com.springBoot.demo.dao.PaperMapper;
 import com.springBoot.demo.pojo.Paper;
+import com.springBoot.util.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
-public class TestController {
+public class TestController extends BaseController {
 
     @Autowired
     PaperMapper paperMapper;
 
     @RequestMapping("/test")
-    public void test(){
-        List<Paper> paperList=paperMapper.selectByMap();
-        System.out.println(paperList.toString());
+    public void test(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse){
+//        List<Paper> paperList=paperMapper.selectByMap();
+//        System.out.println(paperList.toString());
+        Map<String,Object> map=new HashMap<>();
+        getParameterMap(httpServletRequest,map);
+        System.out.println(map.get("value3"));
     }
 }
---
+++ src/main/java/com/springBoot/util/BaseController.java
@@ -0,0 +1,41 @@
+package com.springBoot.util;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class BaseController {
+
+    /**
+     *
+     * @param request 包含所有请求的数据
+     * @param returnMap 用于返回所有数据的表
+     */
+    public static void getParameterMap(HttpServletRequest request,Map returnMap){
+        // 参数Map
+        Map properties = request.getParameterMap();
+        // 返回值Map
+        Iterator entries = properties.entrySet().iterator();
+        Map.Entry entry;
+        String name = "";
+        String value = "";
+        while (entries.hasNext()) {
+            entry = (Map.Entry) entries.next();
+            name = (String) entry.getKey();
+            Object valueObj = entry.getValue();
+            if (null == valueObj) {
+                value = "";
+            } else if (valueObj instanceof String[]) {
+                String[] values = (String[]) valueObj;
+                for (int i = 0; i < values.length; i++) {
+                    value = values[i] + ",";
+                }
+                value = value.substring(0, value.length() - 1);
+            } else {
+                value = valueObj.toString();
+            }
+            returnMap.put(name, value);
+        }
+    }
+}
